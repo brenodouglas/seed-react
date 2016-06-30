b0b21ed3cd259a445b2cd633ae063e4e5040adc2
Breno Douglas <breno.douglas@paieterno.com.br>
2016-06-30 17:35:01 -0300

    Initial app without redux

---
+++ src/containers/App/index.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import styles from './style';
+
+export default class App extends Component
+{
+
+  render()
+  {
+    return (
+      <h1 className={styles.h1}>Hello word</h1>
+    );
+  }
+}
