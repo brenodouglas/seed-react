dd487208ed2ee3a4313f864446fa8ec59693e13b
Breno Douglas <breno.douglas@paieterno.com.br>
2016-07-01 10:36:42 -0300

    Prod configs

---
+++ webpack.prod.config.js
@@ -0,0 +1,67 @@
+const path = require('path');
+const webpack = require('webpack');
+const autoprefixer = require('autoprefixer');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    output: {
+      filename: '[name]-[hash].js',
+      path: path.resolve(__dirname, 'dist'),
+      publicPath: '/'
+    },
+    entry: [
+      './src/app.js'
+    ],
+    module: {
+      loaders: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          loaders: ['babel']
+        },
+        {
+  				test: /\.json$/,
+  				loader: 'json-loader'
+  			},
+        {
+          test: /\.css$/,
+          include: /node_modules/,
+          loader: ExtractTextPlugin.extract('style-loader', 'css-loader?modules&importLoaders=1&sourceMap!postcss-loader')
+        },
+        {
+          test: /\.css$/,
+          exclude: /node_modules/,
+          loader: ExtractTextPlugin.extract('style-loader', 'css-loader?modules&importLoaders=1&sourceMap!postcss-loader')
+        }
+      ]
+    },
+    resolve: {
+  		modulesDirectories: [ 'src', 'node_modules' ],
+  		extensions: ['', '.json', '.js', '.jsx', '.css']
+  	},
+    plugins: [
+      new webpack.HotModuleReplacementPlugin(),
+      new ExtractTextPlugin('[name]-[chunkhash].css', { allChunks: true }),
+      new HtmlWebpackPlugin(),
+      // optimizations
+      new webpack.optimize.DedupePlugin(),
+      new webpack.optimize.OccurenceOrderPlugin(),
+      new webpack.optimize.UglifyJsPlugin({
+        'screw-ie8': true,
+        comments: false,
+        compress: {
+          warnings: false
+        }
+      })
+    ],
+    postcss: [
+      require('postcss-clearfix'),
+      require('postcss-nested'),
+      require('postcss-cssnext')({
+         features: {
+           rem: { rootValue: 10 }
+         }
+       })
+    ]
+};
