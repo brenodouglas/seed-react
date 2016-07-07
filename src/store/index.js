7cf0a76d23cc4ff50455a2ec0fd7b19fa6a9df40
Breno Douglas <breno.douglas@paieterno.com.br>
2016-07-07 08:54:09 -0300

    Add Redux thunk

--- src/store/index.js
+++ src/store/index.js
@@ -2,9 +2,11 @@ import { createStore, applyMiddleware } from 'redux';
 import redurces from './redurces';
 import apiMiddleware from './middlewares/api';
 import fetchMiddleware from './middlewares/fetch';
+import thunk from 'redux-thunk';
 
 export default createStore(
     redurces,
+    applyMiddleware(thunk),
     applyMiddleware(apiMiddleware),
     applyMiddleware(fetchMiddleware)
 );
